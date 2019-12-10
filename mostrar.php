142f415e8b6f11897f51a3a244acfb9704bfb21e
Gaston Arevalo <gastonarevalo@Gastons-MacBook-Pro.local>
2019-12-10 18:47:46 -0300

    modificado los formularios

--- mostrar.php
+++ mostrar.php
@@ -1,21 +1,23 @@
 <?php
-
-
-    echo "HAMBURGUESA 1";
-    $medida = $_POST["medida"]; //Toma el valor de la medida
+    echo $_POST["tomate"];
+    for($i = 0;$i<$cantidad;$i++){
+    echo "HAMBURGUESA ".$i;
+    $medida = $_POST["medida$i"]; //Toma el valor de la medida
     //es la manera de representar un select por medio de recorrer un string;
     for ($i=0;$i<count($medida);$i++){     
         echo "<br> medida" . ": " . $medida[$i]."<br><br>";    
     }
+    
     echo "Toppings de la Hamburguesa: <br><br>";
     
-    echo $_POST["lechuga"]." ";
-    echo $_POST["tomate"]." ";
-    echo $_POST["cebollam"]."<br>";
-    echo $_POST["cebollac"]." ";
-    echo $_POST["chiles"]." ";
-    echo $_POST["bacon"]."<br>";
-    echo $_POST["champ"]." ";
-    echo $_POST["salsa"]." ";
-    echo $_POST["pan"]."<br>";
+    echo $_POST["lechuga$i"]." ";
+    echo $_POST["tomate$i"]." ";
+    echo $_POST["cebollam$i"]."<br>";
+    echo $_POST["cebollac$i"]." ";
+    echo $_POST["chiles$i"]." ";
+    echo $_POST["bacon$i"]."<br>";
+    echo $_POST["champ$i"]." ";
+    echo $_POST["salsa$i"]." ";
+    echo $_POST["pan$i"]."<br>";
+}
  ?>
--- procesar.php
+++ procesar.php
@@ -9,8 +9,9 @@
 <body>
 <form action="mostrar.php" method="POST">
 <?$cantidad = $_POST["cantidad"];?>
+<p>Por favor especifique el pedido</p><br><br>
 <?for($i=0;$i<$cantidad;$i++){?>
- <p>Por favor especifique el pedido</p><br><br>
+ 
 
  TAMAÑO:
  <select name="medida[]" id="">
@@ -21,17 +22,17 @@
  </select>
  <br>
  <label for="toppings">Toppings</label>
- Lechuga                 <input type="checkbox" name="lechuga" value="lechuga">
- Tomate                  <input type="checkbox" name="tomate" value="tomate">
- Cebolla Morada          <input type="checkbox" name="cebollam" value="cebollam"><br>
- Cebolla Caramelizada    <input type="checkbox" name="cebollac" value="cebollac">
- Chiles                  <input type="checkbox" name="chiles" value="chiles">
- Bacon                   <input type="checkbox" name="bacon" value="bacon"><br>
- Champiñones             <input type="checkbox" name="champ" value="champ">
- Salsa Homies            <input type="checkbox" name="salsa" value="salsa">
- Pan Homies              <input type="checkbox" name="pan" value="pan"><hr>
- <input type="submit" value="Enviar">
+ Lechuga                 <input type="checkbox" name="lechuga<?echo $i;?>" value="lechuga">
+ Tomate                  <input type="checkbox" name="tomate<?echo $i;?>" value="tomate">
+ Cebolla Morada          <input type="checkbox" name="cebollam<?echo $i;?>" value="cebollam"><br>
+ Cebolla Caramelizada    <input type="checkbox" name="cebollac<?echo $i;?>" value="cebollac">
+ Chiles                  <input type="checkbox" name="chiles<?echo $i;?>" value="chiles">
+ Bacon                   <input type="checkbox" name="bacon<?echo $i;?>" value="bacon"><br>
+ Champiñones             <input type="checkbox" name="champ<?echo $i;?>" value="champ">
+ Salsa Homies            <input type="checkbox" name="salsa<?echo $i;?>" value="salsa">
+ Pan Homies              <input type="checkbox" name="pan<?echo $i;?>" value="pan"><hr>
  <?}?>
+ <input type="submit" value="Enviar">
  </form>
  
  <br><br>
