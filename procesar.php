d5298b2058bff992867687574c8d6c7f5182593d
Gaston Arevalo <gastonarevalo@Gastons-MacBook-Pro.local>
2019-12-10 18:33:38 -0300

    Agregado los formularios correctamente

--- procesar.php
+++ procesar.php
@@ -1,38 +1,39 @@
-<?php
-$seconds = 5;
-switch($_POST["cantidad"]){
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="mostrar.php" method="POST">
+<?$cantidad = $_POST["cantidad"];?>
+<?for($i=0;$i<$cantidad;$i++){?>
+ <p>Por favor especifique el pedido</p><br><br>
 
-    case 1:
-        header('Location: especificar1.php');
-        break;  
-    case 2:
-        header('Location: especificar2.php');
-        break;
-    case 3:
-        header('Location: especificar3.php');
-        break;  
-    case 4:
-        header('Location: especificar4.php');
-        break;  
-        header('Location: especificar5.php');
-    case 5:
-        header('Location: especificar6.php');
-        break;  
-    case 6:
-        header('Location: especificar7.php');
-        break;
-    case 7:
-        header('Location: especificar8.php');
-        break;  
-    case 8:
-        header('Location: especificar9.php');
-        break;
-    case 9:
-        header('Location: especificar10.php');
-        break;  
-    case 10:
-        header('Location: especificar.php');
-        break;
-    }
-                       
-?>
+ TAMAÑO:
+ <select name="medida[]" id="">
+ <option value="s">S</option>
+ <option value="m">M</option>
+ <option value="l">L</option>
+ <option value="xl">XL</option>
+ </select>
+ <br>
+ <label for="toppings">Toppings</label>
+ Lechuga                 <input type="checkbox" name="lechuga" value="lechuga">
+ Tomate                  <input type="checkbox" name="tomate" value="tomate">
+ Cebolla Morada          <input type="checkbox" name="cebollam" value="cebollam"><br>
+ Cebolla Caramelizada    <input type="checkbox" name="cebollac" value="cebollac">
+ Chiles                  <input type="checkbox" name="chiles" value="chiles">
+ Bacon                   <input type="checkbox" name="bacon" value="bacon"><br>
+ Champiñones             <input type="checkbox" name="champ" value="champ">
+ Salsa Homies            <input type="checkbox" name="salsa" value="salsa">
+ Pan Homies              <input type="checkbox" name="pan" value="pan"><hr>
+ <input type="submit" value="Enviar">
+ <?}?>
+ </form>
+ 
+ <br><br>
+</body>
+</html>
